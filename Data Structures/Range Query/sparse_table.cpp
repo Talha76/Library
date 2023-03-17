e9ff3f3ce7938cfed07d94164031c45b38d53c3d
Talha76 <mushfiqurrahman845@gmail.com>
2023-03-17 15:11:45 +0600

    Updated well-tested sparse table

--- Data Structures/Range Query/sparse_table.cpp
+++ Data Structures/Range Query/sparse_table.cpp
@@ -1,5 +1,39 @@
-#include "bits/stdc++.h"
-using namespace std;
+// tested by: https://judge.yosupo.jp/problem/staticrmq
+
+// 0-based indexing, query finds in range [first, last]
+namespace sparse_table {
+#define __lg(x) (31 - __builtin_clz(x))
+  int n;
+  vector<vector<int>> table;
+
+  int f(int p1, int p2) { return min(p1, p2); }
+
+  void build(vector<int> &a) {
+    n = a.size(); table.assign(n, vector<int>(__lg(n) + 1));
+    for(int i = 0; i < n; i++) table[i][0] = a[i];
+      int lg = __lg(n) + 1;
+    for(int j = 1; j < lg; j++) {
+      for(int i = 0; i + (1<<j) <= n; i++)
+        table[i][j] = f(table[i][j - 1], table[i + (1<<(j - 1))][j - 1]);
+    }
+  }
+
+  int query(int l, int r) {
+    int d = r - l + 1;
+    int ret = INT_MAX;
+    int lg = __lg(d);
+
+    // overlapping queries
+    ret = f(table[l][lg], table[r - (1<<lg) + 1][lg]);
+
+    // Non-overlapping queries
+    for(int i = 0; i <= lg; l += ((d>>i)&1)*(1<<i), i++)
+      if((d >> i) & 1)
+        ret = f(ret, table[l][i]);
+    
+    return ret;
+  }
+};
 
 #define __lg(x) (31 - __builtin_clz(x))
 // 0-based indexing, query finds in range [first, last]
@@ -37,20 +71,3 @@ private:
   }
 };
 
-// Solves: https://www.spoj.com/problems/RMQSQ/
-int main() {
-	int n;
-  cin >> n;
-  vector<int> a(n);
-  for(auto &i : a)
-    cin >> i;
-
-  int q;
-  cin >> q;
-  sparse_table<int> st(a);
-  while(q--) {
-    int l, r;
-    cin >> l >> r;
-    cout << st.query(l, r) << "\n";
-  }
-}
