38a986b724e312fcdd61dc7cf66114f34a0550ed
Talha76 <mushfiqurrahman845@gmail.com>
2023-03-24 17:45:54 +0600

    Updated BigInt to big_int

--- Data Structures/BigInt.cpp
+++ Data Structures/BigInt.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 typedef long long ll;
 
-class BigInt {
-  BigInt multiply(const char c, int n) {
-    BigInt a; int carry = 0;
+class big_int {
+  big_int multiply(const char c, int n) {
+    big_int a; int carry = 0;
     while(n--) a.s += "0";
     for(int i = 0; i < s.size(); i++) {
       int x = (s[i] - 48)*(c - 48) + carry;
@@ -23,10 +23,10 @@ public:
   string s;
 
   // Constructor
-  BigInt() {} // Default
-  BigInt(const string &a) { s = a; reverse(all(s)); normalize(); }
-  BigInt(const int &a) { s = to_string(a); reverse(all(s)); normalize(); }
-  BigInt(const ll &a) { s = to_string(a); reverse(all(s)); normalize(); }
+  big_int() {} // Default
+  big_int(const string &a) { s = a; reverse(all(s)); normalize(); }
+  big_int(const int &a) { s = to_string(a); reverse(all(s)); normalize(); }
+  big_int(const ll &a) { s = to_string(a); reverse(all(s)); normalize(); }
 
   // Normalization
   void normalize() { while(!s.empty() && s.back() == 48) s.pop_back(); }
@@ -35,30 +35,30 @@ public:
   unsigned int size() const { return s.size(); }
 
   // IO Operator
-  friend istream& operator >> (istream &i, BigInt &a)
+  friend istream& operator >> (istream &i, big_int &a)
   { i >> a.s; reverse(all(a.s)); a.normalize(); return i;}
-  friend ostream& operator << (ostream &o, BigInt a)
+  friend ostream& operator << (ostream &o, big_int a)
   { reverse(all(a.s)); o << a.s; return o; }
 
   // Assignment Operator
   void operator = (const string& a) { s = a; }
 
   // Equality Operator
-  bool operator == (const BigInt &a) { return a.s == s; }
-  bool operator != (const BigInt &a) { return a.s != s; }
+  bool operator == (const big_int &a) { return a.s == s; }
+  bool operator != (const big_int &a) { return a.s != s; }
 
   // Relational Operator
-  bool operator > (const BigInt&);
-  bool operator >= (const BigInt&);
-  bool operator < (const BigInt&);
-  bool operator <= (const BigInt&);
+  bool operator > (const big_int&);
+  bool operator >= (const big_int&);
+  bool operator < (const big_int&);
+  bool operator <= (const big_int&);
 
   // Unary Operator
-  BigInt operator - ();
+  big_int operator - ();
 
   // Binary Operator
-  BigInt operator + (BigInt a) {
-    BigInt c; bool carry = false;
+  big_int operator + (big_int a) {
+    big_int c; bool carry = false;
     while(s.size() != a.s.size()) (s.size() < a.s.size() ? s : a.s) += "0";
     for(int i = 0; i < s.size(); i++) {
       int x = carry + s[i] + a.s[i] - 96;
@@ -68,29 +68,29 @@ public:
     c.s += carry + 48; c.normalize(); this->normalize();
     return c;
   }
-  BigInt operator - (BigInt);
-  BigInt operator * (BigInt a) {
-    BigInt c; int pow = 0;
+  big_int operator - (big_int);
+  big_int operator * (big_int a) {
+    big_int c; int pow = 0;
     for(int i = 0; i < a.s.size(); i++) {
-      BigInt prod = multiply(a.s[i], pow++);
+      big_int prod = multiply(a.s[i], pow++);
       c += prod;
     }
     c.normalize();
     return c;
   }
-  BigInt operator / (BigInt);
+  big_int operator / (big_int);
 
   // Some extra operators
-  BigInt operator += (BigInt a) {
+  big_int operator += (big_int a) {
     *this = *this + a;
     return *this;
   }
-  BigInt operator -= (BigInt a);
-  BigInt operator *= (BigInt a) {
+  big_int operator -= (big_int a);
+  big_int operator *= (big_int a) {
     *this = *this * a;
     return *this;
   }
-  BigInt operator /= (BigInt a);
+  big_int operator /= (big_int a);
 };
 
 int main() {
