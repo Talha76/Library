cc96b97a5dbef4a2d3bc77023d1be0d698fe3cc2
Talha76 <mushfiqurrahman845@gmail.com>
2023-08-19 16:55:08 +0600

    Updated smth

--- Data Structures/Range Query/seg_tree.cpp
+++ Data Structures/Range Query/seg_tree.cpp
@@ -1,72 +1,72 @@
-// Then which of Allah's favor will you deny?
-// problem-url: https://www.spoj.com/problems/RMQSQ/
-// author: Mushfiq_Talha
-#ifdef MUSHFIQ_DEBUG
-#include "dbg.h"
-#else
-#define dbg(...) {}
-#endif
-#include "bits/stdc++.h"
-#define all(v) v.begin(), v.end()
-using namespace std;
-typedef long long LL;
-typedef pair<int, int> PII;
-
 // Finds in range [first, last]
 template<typename T> struct seg_tree {
 private:
   int n;
+  LL null;
   vector<T> a, Tree;
 
-  T f(T a, T b) { return min(a, b); }
+  T combine(T a, T b) { return max(a, b); }
 
   T seg_query(const int i, const int j, int l, int r, int node = 1) {
     if(l >= i and r <= j) return Tree[node];
     if(i > r or j < l) return 1e18;
     int m = (l + r) >> 1;
-    return f(seg_query(l, m, i, j, 2*node), seg_query(m + 1, r, i, j, 2*node + 1));
+    return combine(seg_query(l, m, i, j, 2*node), seg_query(m + 1, r, i, j, 2*node + 1));
   }
 
-  T seg_update(const int i, const T v, int l, int r, int node = 1) {
+  void seg_update(const int i, const T v, int l, int r, int node = 1) {
     if(i > r or i < l) return Tree[node];
     if(l == r) return Tree[node] = v;
     int m = (l + r) >> 1;
-    return Tree[node] = f(seg_update(l, m, i, v, 2*node), seg_update(m + 1, r, i, v, 2*node + 1));
+    seg_update(l, m, i, v, 2*node), seg_update(m + 1, r, i, v, 2*node + 1);
+    Tree[n] = combine(Tr[2*node], Tr[2*node + 1]);
   }
+
 public:
-  seg_tree() {}
-  seg_tree(vector<T> &a) : n(a.size()), a(a), Tree(vector<T>(4*a.size())) { a.insert(a.begin(), -1); build(1, n); }
+  seg_tree(vector<T> &a, T _null = 0) : n(a.size()), a(a), Tree(vector<T>(4*a.size())) { a.insert(a.begin(), -1); build(1, n); }
 
   T build(int l, int r, int node = 1) {
     if(l == r) return Tree[node] = a[l];
     int m = (l + r) >> 1;
-    return Tree[n] = f(build(l, m, 2*node), build(m + 1, r, 2*node + 1));
+    return Tree[n] = combine(build(l, m, 2*node), build(m + 1, r, 2*node + 1));
   }
 
-  T query(const int &i, const int &j) { return seg_query(i, j, 1, n); }
+  T query(const int &i, const int &j) { return seg_query(i, j, 0, n-1); }
 
-  T update(const int &i, const int &v) { return seg_update(i, v, 1, n); }
+  T update(const int &i, const int &v) { return seg_update(i, v, 0, n-1); }
 };
 
-signed main() {
-  cin.tie(NULL)->sync_with_stdio(false);
+const int N = 1e6 + 1;
+int a[N], tr[N << 2];
 
-  int n;
-  cin >> n;
-  vector<LL> a(n);
-  for(auto &i : a)
-    cin >> i;
+namespace segTree {
+  using DT = LL;
+  using UT = LL;
   
-  seg_tree<LL> s(a);
-  int q;
-  cin >> q;
-  while(q--) {
-    int i, j;
-    cin >> i >> j;
-    i++; j++;
-    cout << s.query(i, j) << endl;
+  DT null = 0;
+
+  DT merge(DT a, DT b) { return max(a, b); }
+  DT combine(DT a, DT b) { return max(a, b); }
+
+  DT build(int l, int r, int n = 1) {
+    if (l == r)
+      return tr[n] = a[l];
+    int m = l + r >> 1;
+    return tr[n] = merge(tr[2*n], tr[2*n + 1]);
   }
 
-  return 0;
-}
+  void update(int i, UT x, int l, int r, int n = 1) {
+    if (i < l or i > r) return;
+    if (i == l and l == r) return tr[n];
+    int m = l + r >> 1;
+    update(i, x, l, m, 2*n), update(i, x, m+1, r, 2*n + 1);
+    tr[n] = merge(tr[2*n], tr[2*n + 1]);
+  }
 
+  DT query(int ql, int qr, int l, int r, int n = 1) {
+    if (ql > r or qr < l) return null;
+    if (ql <= l and r <= qr) return tr[n];
+    int m = l + r >> 1;
+    return combine(query(ql, qr, l, m, 2*n), query(ql, qr, m+1, r, 2*n + 1));
+  }
+}
--- Data Structures/Range Query/sparse_table.cpp
+++ Data Structures/Range Query/sparse_table.cpp
@@ -1,39 +1,30 @@
 // tested by: https://judge.yosupo.jp/problem/staticrmq
 
 // 0-based indexing, query finds in range [first, last]
-namespace sparse_table {
-#define __lg(x) (31 - __builtin_clz(x))
-  int n;
-  vector<vector<int>> table;
+#define lg(x) (31 - __builtin_clz(x))
 
+const int N = 1e5 + 1;
+const int K = lg(N);
+
+int a[N], tr[N][K + 1];
+
+namespace sparse_table {
   int f(int p1, int p2) { return min(p1, p2); }
 
-  void build(vector<int> &a) {
-    n = a.size(); table.assign(n, vector<int>(__lg(n) + 1));
-    for(int i = 0; i < n; i++) table[i][0] = a[i];
-      int lg = __lg(n) + 1;
-    for(int j = 1; j < lg; j++) {
+  void build() {
+    for(int i = 0; i < n; i++)
+      tr[i][0] = a[i];
+    for(int j = 1; j <= K; j++) {
       for(int i = 0; i + (1<<j) <= n; i++)
-        table[i][j] = f(table[i][j - 1], table[i + (1<<(j - 1))][j - 1]);
+        tr[i][j] = f(tr[i][j - 1], tr[i + (1<<(j - 1))][j - 1]);
     }
   }
 
   int query(int l, int r) {
-    int d = r - l + 1;
-    int ret = INT_MAX;
-    int lg = __lg(d);
-
-    // overlapping queries
-    ret = f(table[l][lg], table[r - (1<<lg) + 1][lg]);
-
-    // Non-overlapping queries
-    for(int i = 0; i <= lg; l += ((d>>i)&1)*(1<<i), i++)
-      if((d >> i) & 1)
-        ret = f(ret, table[l][i]);
-    
-    return ret;
+    int d = lg(r - l + 1);
+    return f(table[l][d], table[r - (1<<d) + 1][d]);
   }
-};
+}
 
 #define __lg(x) (31 - __builtin_clz(x))
 // 0-based indexing, query finds in range [first, last]
@@ -63,7 +54,7 @@ private:
   T f(T p1, T p2) { return min(p1, p2); }
   void build() {
     for(int i = 0; i < n; i++) table[i][0] = a[i];
-      int lg = __lg(n) + 1;
+    int lg = __lg(n) + 1;
     for(int j = 1; j < lg; j++) {
       for(int i = 0; i + (1<<j) <= n; i++)
         table[i][j] = f(table[i][j - 1], table[i + (1<<(j - 1))][j - 1]);
